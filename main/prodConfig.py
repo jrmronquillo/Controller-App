3af23db8b9d95c3dd4d95e69a8e7bed03d6322e5
RONQUILLO <jr9099@dt2017-dfwrtr-002-tlbx.ks.us.att.com>
2018-04-05 14:09:44 -0700

    updated prodConfig with e2ehost_local

--- main/prodConfig.py
+++ main/prodConfig.py
@@ -1,26 +1,26 @@
 config = {
             "screenshot_config" : [
                 {
-                    "command" : "stbt screenshot /home/e2e/e2ehost29_local/sanityAutomation/automation_main_28/images/",
+                    "command" : "stbt screenshot /home/e2e/e2ehost_local/sanityAutomation/automation_main_28/images/",
                 	"defaultCommand" : "stbt screenshot"
                 }
             ],
 
             "testcases_config" : [
             	{
-            		"list_command" : "ls -tr /home/e2e/e2ehost29_local/sanityAutomation/automation_main_28"
+            		"list_command" : "ls -tr /home/e2e/e2ehost_local/sanityAutomation/automation_main_28"
             	}
             ],
 
             "createtestcase_config" : [
             	{
-            		"dir_path" : "/home/e2e/e2ehost29_local/sanityAutomation/automation_main_28/"
+            		"dir_path" : "/home/e2e/e2ehost_local/sanityAutomation/automation_main_28/"
             	}
             ],
 
              "deletetestcase_config" : [
             	{
-            		"delete_command" : "rm -r /home/e2e/e2ehost29_local/sanityAutomation/automation_main_28/"
+            		"delete_command" : "rm -r /home/e2e/e2ehost_local/sanityAutomation/automation_main_28/"
             	}
             ]
         }
