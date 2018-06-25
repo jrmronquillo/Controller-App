f6a96a475fc6d031d1870f3048aa89da7aaa94ea
RONQUILLO <jr9099@ELSML03JR909A-4.local>
2018-06-25 14:53:16 -0700

    changed functionality where when start script is pressed, it goes to the reporting page

--- main/main.py
+++ main/main.py
@@ -763,7 +763,7 @@ def showTestCases():
                     commandString = "stbt run "+path
                     print commandString
                     p = subprocess.Popen(commandString, shell=True)
-                    return render_template('testcases.html', test_cases=test_cases)
+                    return render_template('reporting.html', test_cases=test_cases)
             else:
                 message = "Did not find any matching test cases with that id, did not run script"
                 print message
