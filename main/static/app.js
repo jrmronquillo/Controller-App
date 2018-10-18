7267bed9e70d000cfe9c8631534e8968f2de5964
RONQUILLO <jr9099@ELSML03JR909A-4.local>
2018-10-18 15:37:43 -0700

    configured redesign page with react and babel to use JSX

--- main/static/app.js
+++ main/static/app.js
@@ -2,24 +2,21 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    return(
+      <div>
+        <h1>rendered</h1>
+        <button>Test</button>
+      </div>
     );
   }
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(< Main />, domContainer);
